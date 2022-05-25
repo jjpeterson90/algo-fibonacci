548bc9abcaaec606f89de79d228a2c6da27d9557
Justin <jjpeterson90@hotmail.com>
2022-05-25 14:08:25 -0500

    complete

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,10 @@
+fib_memo = {}
+
 def fibonacci(n):
-  pass
-
-
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  if n not in fib_memo:
+    fib_memo[n] = fibonacci(n-1) + fibonacci(n-2)
+  return fib_memo[n]
